afd81d8f4b8f0d9d72f5145b93fe559babe148f0
Erik R. Jensen <ejensen@netradius.com>
2016-12-28 19:55:54 -0700

    Adding initial spring boot sources

---
+++ src/main/java/com/authsure/demo/AuthsureSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.authsure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsureSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsureSpringDemoApplication.class, args);
+	}
+}
